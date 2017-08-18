c0b18873ad2f84d41fb262ebb5a2a7bd6ee06c84
Briancas20 <briancastaneda@MacBook-Pro-7.attlocal.net>
2017-08-17 17:59:32 -0700

    snap apps
    snap apps

---
+++ Assets/js/custom-js.js
@@ -0,0 +1,55 @@
+﻿$('button').sideNav({
+    menuWidth:260,
+    edge: 'left', // this is the horizontal origin 
+    closeOnClick: false,/// close the side nav on <a>, useful for angular
+    draggable: true// wheather you can drag to open touch screens 
+});
+
+$('.modal').modal({
+    dismissible: true, //Modals can be dismissed by clicking the the outside of the model 
+    opacity: .5, // this handles the opacity 
+    inDuration: 300,// transition duration 
+    outDuration: 200, // transition duration 
+    startingTop: '4%', // starting top style attributes
+    endingtop: '10%',
+    ready: function (modal, trigger) { // here we will call back for the model and trigger parameters that available
+
+    },
+    complete: function () { } // call back for the modal to close
+});
+
+$('.collapsible').collapsible({
+
+    accordion: false,  // a setting that changes the collapsible behavior of the expandable accordion style.
+    onOpen: function (el) { }, // callback for collapsable open
+    onClose: function (el) { } //callback for collapsable close 
+
+});
+$(document).ready(function () {
+
+    $('select').material_select();
+
+});
+$('#feedback').val(' ');
+$('#feedback').trigger('autoresize');
+
+
+$("#search").on("keyup", function () {
+    var value = $(this).val();
+
+    $("table tr").each(function (index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td:first").text();
+
+            if (id.indexOf(value) !== 0) {
+                $row.hide();
+            }
+            else {
+                $row.show();
+            }
+        }
+    });
+});
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Playground.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact paged!!!!";
+
+            return View();
+        }
+        public ActionResult Duplicates()
+        {
+            ViewBag.Message = "Your duplicates page";
+            return View();
+        }
+        public ActionResult RecentlyContacted()
+        {
+            ViewBag.Message = " Your recently contacted page";
+            return View();
+        }
+        public ActionResult Groups()
+        {
+            ViewBag.Message = "Your recent events";
+                return View();
+        }
+        public ActionResult Day()
+        {
+            ViewBag.Message = "Your recent day events";
+            return View();
+        }
+    }
+}
